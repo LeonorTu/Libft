a49ae772fc9d94b422d2be629803983e8d0f3e50
JingyiTu/Leonor <106062582+LeonorTu@users.noreply.github.com>
2023-11-27 22:05:06 +0200

    Update get_next_line.c

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -15,13 +15,16 @@
 char	*get_next_line(int fd)
 {
 	static char	buf;
-	char	line;
+	char	*line;
 
 	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, &buf, 0) < 0)
 		return (NULL);
 	while ()
 	{
-
+		if (read(fd, buf, BUFFER_SIZE) <= 0)
+			free(buf);
+			return ();
+		
 	}
 	return (line);
 }
