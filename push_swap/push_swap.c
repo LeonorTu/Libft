15fec907acae059a5d488a9b615d1336e31f6334
Jingyi Tu <jingyi.tu@outlook.com>
2023-12-29 17:50:45 +0200

    edit

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -6,24 +6,13 @@
 /*   By: jtu <jtu@student.hive.fi>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/21 16:25:59 by jtu               #+#    #+#             */
-/*   Updated: 2023/12/22 20:13:57 by jtu              ###   ########.fr       */
+/*   Updated: 2023/12/29 17:50:09 by jtu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-
-// # include "./libft/libft.h" //
-// # include <stdbool.h> //
-// # include <stdlib.h> //
-// # include <limits.h> //
-// # include <stdio.h> //
-
-typedef struct s_stack{
-	int	value;
-	struct s_stack *prev;
-	struct s_stack *next;
-}	t_stack;
+# include <stdio.h> //
 
 bool	stack_sorted(t_stack *stack)
 {
@@ -106,7 +95,7 @@ void	add_node(t_stack **stack, int n)
 	temp->next = new_node;
 }
 
-void init_stack(t_stack **stack, char **argv)
+void	init_stack(t_stack **stack, char **argv)
 {
 	int	i;
 	int	n;
@@ -142,9 +131,141 @@ int	stack_len(t_stack *stack)
 
 void	swap_node(t_stack **stack)
 {
+	t_stack	*temp;
+
+	temp = *stack;
 	*stack = (*stack)->next;
-	(*stack)->prev->prev = *stack;
-	(*stack)->
+	temp->next = (*stack)->next;
+	(*stack)->next = temp;
+}
+
+t_stack	*last_node(t_stack **stack)
+{
+	while ((*stack)->next)
+		*stack = (*stack)->next;
+	return (*stack);
+}
+
+void	sa(t_stack **a)
+{
+	swap_node(a);
+	write(1, "sa\n", 3);
+}
+
+void	sb(t_stack **b)
+{
+	swap_node(b);
+	write(1, "sb\n", 3);
+}
+
+void	ss(t_stack **a, t_stack **b)
+{
+	swap_node(a);
+	swap_node(b);
+	write(1, "ss\n", 3);
+}
+
+void rotate_stack(t_stack **stack)
+{
+	t_stack	*temp;
+
+	if (!*stack || !(*stack)->next)
+		return ;
+	temp = *stack;
+	*stack = last_node(stack);
+	(*stack)->next = temp;
+	*stack = temp->next;
+	temp->next = NULL;
+}
+
+void	ra(t_stack **a)
+{
+	rotate_stack(a);
+	write(1, "ra\n", 3);
+}
+
+void	rb(t_stack **b)
+{
+	rotate_stack(b);
+	write(1, "rb\n", 3);
+}
+
+void	rr(t_stack **a, t_stack **b)
+{
+	rotate_stack(a);
+	rotate_stack(b);
+	write(1, "rr\n", 3);
+}
+
+void	reverse_rotate_stack(t_stack **stack)
+{
+	t_stack	*temp;
+	t_stack	*last;
+
+	if (!*stack || !(*stack)->next || !(*stack)->next->next)
+		return ;
+	temp = *stack;
+	while ((*stack)->next->next)
+		*stack = (*stack)->next;
+	last = (*stack)->next;
+	(*stack)->next = NULL;
+	last->next = temp;
+	*stack = last;
+}
+
+void	rra(t_stack **a)
+{
+	reverse_rotate_stack(a);
+	write(1, "rra\n", 3);
+}
+
+void	rrb(t_stack **b)
+{
+	reverse_rotate_stack(b);
+	write(1, "rrb\n", 3);
+}
+
+void	rrr(t_stack **a, t_stack **b)
+{
+	reverse_rotate_stack(a);
+	reverse_rotate_stack(b);
+	write(1, "rrr\n", 3);
+}
+
+t_stack	*find_biggest(t_stack *stack)
+{
+	int	biggest;
+	t_stack	*biggest_node;
+
+	biggest = INT_MIN;
+	while (stack)
+	{
+		if (stack->value > biggest)
+		{
+			biggest = stack->value;
+			biggest_node = stack;
+		}
+		stack = stack->next;
+	}
+	return (biggest_node);
+}
+
+void	small_sort(t_stack **a)
+{
+	t_stack	*biggest_node;
+
+	biggest_node = find_biggest(*a);
+	if (*a == biggest_node)
+		ra(a);
+	else if ((*a)->next == biggest_node)
+		rra(a);
+	if ((*a)->value > (*a)->next->value)
+		sa(a);
+}
+
+void	push_swap(t_stack **a, t_satck **b)
+{
+	
 }
 
 int	main(int argc, char **argv)
@@ -166,15 +287,18 @@ int	main(int argc, char **argv)
 	if (!stack_sorted(a))
 	{
 		if (stack_len(a) == 2)
-			sa(a);
-
+			sa(&a);
+		else if (stack_len(a) == 3)
+			small_sort(&a);
+		else
+			push_swap(&a, &b);
 	}
 
-	// while (a != NULL) //
-	// {
-	// 	printf("the value of the node: %d\n", a->value);
-	// 	a = a->next;
-	// }
+	while (a != NULL) //
+	{
+		printf("the value of the node: %d\n", a->value);
+		a = a->next;
+	}
 
 	free_stack(&a);
 	return (0);
--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: jtu <jtu@student.hive.fi>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/21 15:56:50 by jtu               #+#    #+#             */
-/*   Updated: 2023/12/22 19:35:50 by jtu              ###   ########.fr       */
+/*   Updated: 2023/12/29 17:13:39 by jtu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,37 @@
 
 typedef struct s_stack{
 	int	value;
-	s_stack *prev;
-	s_stack *next;
+	struct s_stack *next;
 }	t_stack;
 
 // Errors handling
+void	free_stack(t_stack **stack);
+void	free_var(t_stack **stack);
+int	invalid_argv(char *str);
+int	duplicated_argv(t_stack *stack, int n);
+
+// Stack initiation
+void	add_node(t_stack **stack, int n);
+void	init_stack(t_stack **stack, char **argv);
+
+// Stack util
+bool	stack_sorted(t_stack *stack);
+int	stack_len(t_stack *stack);
+void	swap_node(t_stack **stack);
+t_stack	*last_node(t_stack **stack);
+void	rotate_stack(t_stack **stack);
+void	reverse_rotate_stack(t_stack **stack);
 
 // Commands
-void ft_sa();
+void	sb(t_stack **b);
+void	sb(t_stack **b);
+void	ss(t_stack **a, t_stack **b);
+void	ra(t_stack **a);
+void	rb(t_stack **b);
+void	rr(t_stack **a, t_stack **b);
+void	rra(t_stack **a);
+void	rrb(t_stack **b);
+void	rrr(t_stack **a, t_stack **b);
 
 // Algorithms
 
